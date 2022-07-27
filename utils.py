2ed773b2b5894b4fa9200d8a53a0428c0c37c882
Harrison Froedge <harrison.froedge@gmail.com>
2022-07-27 12:12:26 +0200

    added get_net_incomes

--- utils.py
+++ utils.py
@@ -8,3 +8,22 @@ def get_client(clients:pd.DataFrame, account_number:int)->pd.DataFrame:
     return row containing client with account number
     """
     return clients.loc[clients["account_number"] == account_number]
+
+def get_net_incomes(trx:pd.DataFrame)->pd.Series:
+	"""
+	Returns series <sr> with net incomes of all client account ids
+	sr.loc[acct_number] gives associated net income
+	"""
+	debits_and_credits = trx.groupby(['account_number', 'incoming'])['amount'].sum()
+	df = debits_and_credits
+	acct_nets = {acct: int(f.loc[acct][1] - f.loc[acct][0]) for acct in clients["account_number"]}
+
+	index = []
+	nets = []
+	for acct, net_income in acct_nets.items():
+	    index.append(acct)
+	    nets.append(net_income)
+
+    net_incomes = pd.Series(data=nets, index=index)
+
+    return net_incomes
