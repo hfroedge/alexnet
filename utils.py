2169e48fb7c1cebbb9a75f79bffb16bf573e105a
Oskar Boer <oskarboer@gmail.com>
2022-07-28 17:53:50 +0200

    forgot to add modified utils.py file

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def read_csv(folder:str="B", name:str="trx")->pd.DataFrame:
-    return pd.read_csv(f"../uploads_{folder}/{name}.csv")
+	return pd.read_csv(f"../uploads_{folder}/{name}.csv")
 
 def read_products(folder:str="B")->pd.DataFrame:
 	txt = pd.read_csv(f"../uploads_{folder}/products.txt", sep=" ")
@@ -12,7 +12,6 @@ def read_products(folder:str="B")->pd.DataFrame:
 	return pd.DataFrame(data={"products":products, "rewards":rewards})
 
 def convert_features(trx:pd.DataFrame, clients:pd.DataFrame)->pd.DataFrame:
-
 	categ = set(trx.value_counts('description').index)
 
 	clothes = 'clothes'
@@ -81,22 +80,59 @@ def get_net_incomes(trx:pd.DataFrame, clients:pd.DataFrame)->pd.Series:
 	return net_incomes
 
 def __get_recommendation(client:pd.DataFrame, features:pd.DataFrame, acct_num:int)->str:
-    """
-    Given client row, gives recommendation 
-    
-    :param client: row from clients csv
-    :param features: consolidated data of all clients
-    
-    :return recommendation
-    """
-    if client["has_mortgage"].values[0] == 1:
-        recommendation =  "refinance_mortgage"
-    elif features.loc[acct_num]["crypto"] > 0.19:
-        recommendation = "get_investments"
-    else:
-    	recommendation = ""
-        
-    return recommendation
+	"""
+	Given client row, gives recommendation 
+
+	:param client: row from clients csv
+	:param features: consolidated data of all clients
+
+	:return recommendation
+	"""
+	# if client["has_mortgage"].values[0] == 1:
+	#     recommendation =  "refinance_mortgage"
+	# elif features.loc[acct_num]["crypto"] > 0.19:
+	#     recommendation = "get_investments"
+	# else:
+	# 	recommendation = ""
+
+	# Hand picked values which connect feature with product
+	clothes = 'clothes'
+	income = 'income'
+	utils = 'utilities'
+	crypto = 'crypto'
+	car = 'car'
+	food = 'food'
+	telecom = 'telecom'
+	house_fee = 'house_fee'
+	health = 'health'
+	education = 'education'
+	shopping = 'shopping'
+	mortage = 'has_mortgage'
+	home_insurance = 'has_home_insurance'
+	car_insurance = 'has_car_insurance'
+
+
+	meaning = {
+		telecom: ["switch_telco", 80],
+		home_insurance: ["switch_home_insurance", 100],
+		mortage: ["refinance_mortgage", 850],
+		house_fee: ["get_home_insurance", 200],
+		crypto: ["get_investments", 900],
+		car: ["get_car_insurance", 300],
+		utils: ["switch_utils", 100],
+	}
+
+	# chosen features
+	crutial = [car, utils, crypto, telecom, house_fee, mortage, home_insurance]
+
+	# generating score for each important feature
+	scores = []
+	for i in crutial:
+		scores.append(features.loc[acct_num][i] * meaning[i][1])
+
+	if max(scores) > 45:
+		return str(meaning[crutial[scores.index(max(scores))]][0])
+	return ""
 
 def write_solution(clients:pd.DataFrame, features:pd.DataFrame, name:str="tests")->None:
     
@@ -115,11 +151,11 @@ def write_solution(clients:pd.DataFrame, features:pd.DataFrame, name:str="tests"
 
     
 	with open(save_path,"w") as f:
-	    f.write(str(len(recommendations)))
-	    f.write("\n")
-	    for acct, rec in zip(accounts, recommendations):
-	        f.write(str(acct))
-	        f.write(" ")
-	        f.write(rec)
-	        f.write("\n")
+		f.write(str(len(recommendations)))
+		f.write("\n")
+		for acct, rec in zip(accounts, recommendations):
+			f.write(str(acct))
+			f.write(" ")
+			f.write(rec)
+			f.write("\n")
 
